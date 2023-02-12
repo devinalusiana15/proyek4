026fc393b006802c476ab5e8c216e00ccd880341
Devina Lusiana <devinalusiana15@gmail.com>
2023-02-13 06:43:51 +0700

    First

---
+++ app/src/main/java/com/example/home/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.home.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.background = null
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.account -> replaceFragment(Account())
+                R.id.fab -> replaceFragment(Absen())
+
+                else ->{
+
+
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/example/home/ModulHindu.kt
@@ -0,0 +1,11 @@
+package com.example.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModulHindu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modul_hindu)
+    }
+}
